feba712d232c3dc14a7163cefd69d06e9c7f470b
SenpaiYG <yaxye_yssss@hotmail.com>
2022-07-25 17:02:03 +0100

    Change

---
+++ public/js/caseSwitcher.js
@@ -0,0 +1,17 @@
+document.getElementById('quite-btn').addEventListener('click', makeQuiet)
+function makeQuiet(a) {
+    console.log(a)
+    const p = document.getElementById('patrick')
+    let text = p.innerText
+    p.innerText = text.toLowerCase()
+}
+
+document.getElementById('loud-btn').addEventListener('click', louder)
+function louder(b) {
+    console.log(b)
+    const c = document.getElementById('patrick')
+    let text = c.innerText
+    c.innerText = text.toUpperCase()
+}
+
+
